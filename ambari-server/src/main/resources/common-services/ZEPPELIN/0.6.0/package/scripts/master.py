2c5e626b471580ddc307c7e5f1188b7b91174b84
maocorte <mauro.cortellazzi@radicalbit.io>
2016-04-21 09:27:56 +0200

    stack refactory

---
+++ ambari-server/src/main/resources/common-services/ZEPPELIN/0.6.0/package/scripts/master.py
@@ -0,0 +1,98 @@
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+import glob
+from resource_management import *
+
+class Zeppelin(Script):
+
+  def install(self, env):
+    import params
+    self.install_packages(env)
+    #self.create_hdfs_user(params.zeppelin_user, params.spark_jar_dir)
+
+    Directory([params.zeppelin_pid_dir, params.zeppelin_log_dir],
+            owner=params.zeppelin_user,
+            group=params.user_group,
+            recursive=True
+    )
+
+  # def create_hdfs_user(self, user, spark_jar_dir):
+  #   Execute('hadoop fs -mkdir -p /user/'+user, user='hdfs', ignore_failures=True)
+  #   Execute('hadoop fs -chown ' + user + ' /user/'+user, user='hdfs')
+  #   Execute('hadoop fs -chgrp ' + user + ' /user/'+user, user='hdfs')
+  #
+  #   Execute('hadoop fs -mkdir -p '+spark_jar_dir, user='hdfs', ignore_failures=True)
+  #   Execute('hadoop fs -chown ' + user + ' ' + spark_jar_dir, user='hdfs')
+  #   Execute('hadoop fs -chgrp ' + user + ' ' + spark_jar_dir, user='hdfs')
+
+  def configure(self, env):
+    import params
+    env.set_params(params)
+
+    Execute(format('chown -R {zeppelin_user}:{user_group} {zeppelin_dir}'), user='root')
+
+    #write out zeppelin-site.xml
+    XmlConfig("zeppelin-site.xml",
+            conf_dir = params.conf_dir,
+            configurations = params.config['configurations']['zeppelin-site'],
+            owner=params.zeppelin_user,
+            group=params.user_group
+    )
+    #write out zeppelin-env.sh
+    File(
+        format("{params.conf_dir}/zeppelin-env.sh"),
+        owner=params.zeppelin_user,
+        mode=0775,
+        content=Template('zeppelin-env.sh.j2', conf_dir=params.conf_dir)
+    )
+    #write out interpreter.json
+    File(
+        format("{params.conf_dir}/interpreter.json"),
+        owner=params.zeppelin_user,
+        mode=0755,
+        content=Template('interpreter.json', conf_dir=params.conf_dir)
+    )
+    # env_content=InlineTemplate(params.zeppelin_env_content)
+    # File(format("{params.conf_dir}/zeppelin-env.sh"), content=env_content, owner=params.zeppelin_user, group=params.user_group) # , mode=0777)
+
+
+  def stop(self, env):
+    import params
+    Execute (params.zeppelin_dir+'/bin/zeppelin-daemon.sh stop', user=params.zeppelin_user)
+
+
+  def start(self, env):
+    import params
+    self.configure(env)
+
+    Execute (params.zeppelin_dir+'/bin/zeppelin-daemon.sh start', user=params.zeppelin_user)
+    Execute(
+        "echo `ps -A -o pid,command | grep -i \"[j]ava\" | grep org.apache.zeppelin.server.ZeppelinServer | awk '{print $1}'`> " + params.zeppelin_pid_dir + "/zeppelin.pid",
+        user=params.zeppelin_user
+    )
+
+  def status(self, env):
+    import status_params
+    env.set_params(status_params)
+    pid_file = glob.glob(status_params.zeppelin_pid_dir + '/zeppelin.pid')[0]
+    check_process_status(pid_file)
+
+
+if __name__ == "__main__":
+  Zeppelin().execute()
---
+++ ambari-server/src/main/resources/common-services/ZEPPELIN/0.6.0/package/scripts/params.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+from resource_management import *
+import sys, os, re
+
+# server configurations
+config = Script.get_config()
+
+# params from zeppelin-config
+#zeppelin_port = str(config['configurations']['zeppelin-env']['zeppelin_port'])
+
+# params from zeppelin-env
+zeppelin_user= config['configurations']['zeppelin-env']['zeppelin_user']
+user_group = config['configurations']['cluster-env']['user_group']
+zeppelin_log_dir = config['configurations']['zeppelin-env']['zeppelin_log_dir']
+zeppelin_pid_dir = config['configurations']['zeppelin-env']['zeppelin_pid_dir']
+zeppelin_hdfs_user_dir = format("/user/{zeppelin_user}")
+
+zeppelin_dir = '/usr/lib/zeppelin'
+conf_dir = zeppelin_dir + '/conf'
+
+hadoop_conf_dir = conf_select.get_hadoop_conf_dir()
+
+flink_hosts = default('/clusterHostInfo/flink_master_hosts', None)
+alluxio_master_hosts = default('/clusterHostInfo/alluxio_master_hosts', None)
+cassandra_seeds = default('/clusterHostInfo/cassandra_seed_hosts', None)
+cassandra_nodes = default('/clusterHostInfo/cassandra_node_hosts', None)
+
+if flink_hosts is not None:
+  has_flink_master = True
+  flink_conf_dir = '/etc/flink/conf'
+  flink_user = config['configurations']['flink-env']['flink_user']
+else:
+  has_flink_master = False
+
+if alluxio_master_hosts is not None:
+  alluxio_master_host = alluxio_master_hosts[0]
+else:
+  alluxio_master_host = 'localhost'
+
+if cassandra_seeds is not None:
+  if cassandra_nodes is not None:
+    cassandra_all_hosts = cassandra_seeds + cassandra_nodes
+    cassandra_hosts = ','.join(cassandra_all_hosts)
+  else:
+    cassandra_hosts = ','.join(cassandra_seeds)
+else:
+  cassandra_hosts = 'localhost'
+
+#zeppelin-env.sh
+# zeppelin_env_content = config['configurations']['zeppelin-env']['content']
+
+
+#detect configs
+java64_home = config['hostLevelParams']['java_home']
