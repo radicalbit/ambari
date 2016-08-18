2ca23a64f468171a3f759bbbb350b88f112134d6
Mauro Cortellazzi <mauro.cortellazzi@radicalbit.io>
2016-08-19 00:25:29 +0200

    started refactory

---
+++ ambari-server/src/main/resources/common-services/ELASTICSEARCH/2.3.5/package/scripts/elastic_component.py
@@ -0,0 +1,65 @@
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+from resource_management import *
+import signal
+import sys
+import os
+from os.path import isfile
+
+from elasticsearch  import elasticsearch
+
+class ElasticComponent(Script):
+
+    def install(self, env):
+        import params
+        env.set_params(params)
+        self.install_packages(env)
+
+    def configure(self, env):
+        import params
+        env.set_params(params)
+        elasticsearch()
+
+    def stop(self, env):
+        import params
+        env.set_params(params)
+        # TODO: use script instead of service
+        stop_cmd = format("service elasticsearch stop")
+        Execute(stop_cmd)
+
+    def start(self, env):
+        import params
+        env.set_params(params)
+        self.configure(env)
+        # TODO: use script instead of service
+        start_cmd = format("service elasticsearch start")
+        Execute(start_cmd)
+
+    def status(self, env):
+        import params
+        env.set_params(params)
+        # TODO: use pid file instead of service
+        status_cmd = format("service elasticsearch status")
+        Execute(status_cmd)
+
+if __name__ == "__main__":
+    ElasticComponent().execute()
+
+
