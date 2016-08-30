528c7e462fd7a418e5ab2b34bb8ea7d390ccfc35
Mauro Cortellazzi <mauro.cortellazzi@radicalbit.io>
2016-08-30 09:44:56 +0200

    cleaned code

--- ambari-server/src/main/resources/common-services/ELASTICSEARCH/2.3.5/package/scripts/elastic_slave.py
+++ ambari-server/src/main/resources/common-services/ELASTICSEARCH/2.3.5/package/scripts/elastic_slave.py
@@ -32,38 +32,3 @@ class ElasticSlave(ElasticComponent):
 
 if __name__ == "__main__":
     ElasticSlave().execute()
-
-# from slave  import slave
-#
-#
-# class Elasticsearch(Script):
-#     def install(self, env):
-#         import params
-#         env.set_params(params)
-#         print 'Install the Slave'
-#         self.install_packages(env)
-#     def configure(self, env):
-#         import params
-#         env.set_params(params)
-#         slave()
-#     def stop(self, env):
-#         import params
-#         env.set_params(params)
-#         stop_cmd = format("service elasticsearch stop")
-#         Execute(stop_cmd)
-#         print 'Stop the Slave'
-#     def start(self, env):
-#         import params
-#         env.set_params(params)
-#         self.configure(env)
-#         start_cmd = format("service elasticsearch start")
-#         Execute(start_cmd)
-#         print 'Start the Slave'
-#     def status(self, env):
-#         import params
-#         env.set_params(params)
-#         status_cmd = format("service elasticsearch status")
-#         Execute(status_cmd)
-#         print 'Status of the Slave'
-# if __name__ == "__main__":
-#     Elasticsearch().execute()
